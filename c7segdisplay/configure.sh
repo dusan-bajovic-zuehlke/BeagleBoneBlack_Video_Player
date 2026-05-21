79d242ff3117a1a4b4db57767302412f7668c891
Marija Grekulovic <grekulovicmarija@gmail.com>
2026-05-21 15:56:28 +0200

    add autolog and improve boot time

--- c7segdisplay/configure.sh
+++ c7segdisplay/configure.sh
@@ -5,3 +5,19 @@ sudo apt install libavformat-dev -y
 sudo apt install libswscale-dev -y
 sudo apt install libavcodec-dev -y
 sudo apt install libavutil-dev -y
+
+
+sudo systemctl disable keyboard-setup
+sudo systemctl disable systemd-resolved
+sudo systemctl set-default multi-user.target
+
+
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d/
+
+sudo tee /etc/systemd/system/getty@tty1.service.d/override.conf << EOF 
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin $USER --noclear %I \$TERM
+EOF
+
+sudo systemctl daemon-reload
